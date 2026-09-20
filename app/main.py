aeeb6df0175c7b6d3d3ec9f70260946b0f730a49
Larry Wu <larrywutoronto@gmail.com>
2026-09-20 11:29:05 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -48,7 +48,7 @@ def main():
             case _:
                 exec_path = get_executable(path_directories, command)
                 if exec_path:
-                    subprocess.run(args[:], shell=True)
+                    subprocess.run([command, *args[1:]], shell=True)
                 else:
                     sys.stdout.write(f"{line}: command not found\n")
             
